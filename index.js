1dbd166167afc5b8924c1acb61e4c12ae6f31d64
Paliya Dipak <dipakthakor1938@gmail.com>
2024-06-15 12:27:41 +0530

    task 1 completed

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bharatintern');
+const connection = mongoose.connection;
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pages/registration.html',);
+});
+
+const schema = mongoose.Schema;       //defining schema
+const employeeschema = new schema({
+    firstname: String,
+    middlename: String,
+    lastname: String,
+    email: String,
+    password: String,
+    contactno: Number,
+    dob: Date,
+    nationality: String,
+    qualification: String,
+    address: String
+});
+
+const employee = mongoose.model('employee', employeeschema);   //compiling schema to model
+app.post('/register', (req, res) => {
+    
+    data = req.body;
+
+    try {
+        const newemployee = new employee({
+            firstname: data.firstname,
+            middlename: data.middlename,
+            lastname: data.lastname,
+            email: data.email,
+            password: data.password,
+            contactno: data.contactno,
+            dob: data.dob,
+            nationality: data.nationality,
+            qualification: data.qualification,
+            address: data.address
+        })
+
+        newemployee.save();         //storing into the mongoDB database
+        res.sendFile(__dirname + '/pages/success.html');   //redirecting to the success page
+    }
+    catch {
+        res.sendFile(__dirname + '/pages/error.html');
+    }
+
+})
+
+app.listen(port, () => {
+    console.log('Server started on http://localhost:3000');
+});
